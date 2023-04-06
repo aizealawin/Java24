b4960f9a4c6199dcfb4b249cdbc9086d3a36c6a4
aizealawin <ktmudalige@gmail.com>
2023-04-06 09:40:56 -0400

    Credits Exercise

---
+++ Java24/src/com/java24hours/BlankFiller.java
@@ -0,0 +1,11 @@
+package com.java24hours;
+
+import java.lang.reflect.Array;
+
+public class BlankFiller {
+  public static void main(String[] arguments) {
+
+    System.out
+        .prtinln("The " + arguments[0] + " " + arguments[1] + " fox " + "jumped over the " + arguments[2] + " dog.");
+  }
+}
