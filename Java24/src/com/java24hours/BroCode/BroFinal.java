a9aefd03e998a1821db7275e3b6c9f6c3d35b4a3
aizealawin <ktmudalige@gmail.com>
2023-04-24 14:08:54 -0400

    final

---
+++ Java24/src/com/java24hours/BroCode/BroFinal.java
@@ -0,0 +1,16 @@
+package com.java24hours.BroCode;
+
+import java.util.*;
+
+public class BroFinal {
+  public static void main(String[] args) {
+
+    final double PI = 3.14159;
+
+    pi = 4;
+
+    System.out.println(pi);
+
+    // anything that is final cannot be changed.
+  }
+}
