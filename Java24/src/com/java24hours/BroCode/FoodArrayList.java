573c023c83e74019cca1d158ba16129beefc69f7
aizealawin <ktmudalige@gmail.com>
2023-04-24 11:42:08 -0400

    arrayList

---
+++ Java24/src/com/java24hours/BroCode/FoodArrayList.java
@@ -0,0 +1,21 @@
+package com.java24hours.BroCode;
+
+import java.util.ArrayList;
+
+public class FoodArrayList {
+  public static void main(String[] args) {
+    ArrayList<String> food = new ArrayList<String>();
+
+    food.add("pizza");
+    food.add("hamburger");
+    food.add("hotdog");
+
+    food.set(0, "sushi");
+    food.remove(2);
+    food.clear();
+
+    for (int i = 0; i < food.size(); i++) {
+      System.out.println(food.get(i));
+    }
+  }
+}
