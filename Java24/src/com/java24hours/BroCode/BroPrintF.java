fa0484e2fea331a0d512ef3faa7870d65c6e7497
aizealawin <ktmudalige@gmail.com>
2023-04-24 14:06:55 -0400

    printf syntax

--- Java24/src/com/java24hours/BroCode/BroPrintF.java
+++ Java24/src/com/java24hours/BroCode/BroPrintF.java
@@ -18,11 +18,32 @@ public class BroPrintF {
     int myInt = 12;
     double myDouble = 1000;
 
-    System.out.printf("%b", myBoolean);
-    System.out.printf("%c", myChar);
-    System.out.printf("%s", myString);
-    System.out.printf("%d", myInt);
-    System.out.printf("%f", myDouble);
+    // System.out.printf("%b", myBoolean);
+    // System.out.printf("%c", myChar);
+    // System.out.printf("%s", myString);
+    // System.out.printf("%d", myInt);
+    // System.out.printf("%f", myDouble);
+
+    // [width]
+    // minimum number of characters to be written as output
+    // negative left justifies the text
+
+    // System.out.printf("Hello %10s", myString);
+
+    // [precision]
+    // sets a number of digits of preciison when outputting floating-point values
+
+    // System.out.printf("You have this much money %.2f", myDouble);
+
+    // [flags]
+    // adds an effect to output based on flag added to format specifier
+    // - : left justify
+    // + : output a + or - sign for numeric value
+    // 0 : numeric values are zero-padded
+    // , : comma grouping separator if numbers > 1000
+
+    System.out.printf("You have this much money %,.2f", myDouble);
+
   }
 
 }
