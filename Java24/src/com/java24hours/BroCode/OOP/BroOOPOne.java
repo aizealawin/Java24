8a6bcfdb37b634a64f93497a3bb3a7390e661ff4
aizealawin <ktmudalige@gmail.com>
2023-04-24 14:13:32 -0400

    oop bro code

---
+++ Java24/src/com/java24hours/BroCode/OOP/BroOOPOne.java
@@ -0,0 +1,11 @@
+package com.java24hours.OOP;
+
+public class BroOOPOne {
+  public static void main(String[] args) {
+
+    // object = an instance of a class that may contain attributes and methods
+    // example: (phone, desk, computer, coffee cup)
+
+  }
+
+}
