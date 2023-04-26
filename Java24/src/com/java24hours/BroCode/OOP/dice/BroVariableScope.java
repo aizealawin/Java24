d46007bb228b9612ce5e7f234862910d03a6f139
aizealawin <ktmudalige@gmail.com>
2023-04-26 15:20:25 -0400

    Global and Local Scopes

---
+++ Java24/src/com/java24hours/BroCode/OOP/dice/BroVariableScope.java
@@ -0,0 +1,9 @@
+package com.java24hours.BroCode.OOP.dice;
+
+public class BroVariableScope {
+  public static void main(String[] args) {
+
+    DiceRoller diceRoller = new DiceRoller();
+
+  }
+}
---
+++ Java24/src/com/java24hours/BroCode/OOP/dice/DiceRoller.java
@@ -0,0 +1,19 @@
+package com.java24hours.BroCode.OOP.dice;
+
+import java.util.Random;
+
+public class DiceRoller {
+  Random random;
+  int number;
+
+  DiceRoller() {
+    random = new Random();
+    roll();
+  }
+
+  void roll() {
+    number = random.nextInt(6) + 1;
+    System.out.println(number);
+
+  }
+}
