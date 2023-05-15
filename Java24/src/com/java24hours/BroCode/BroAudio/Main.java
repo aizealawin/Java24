58636ba847a73415e53281b488c158b797c02753
aizealawin <ktmudalige@gmail.com>
2023-05-15 13:05:54 -0400

    made an interface within the console

--- Java24/src/com/java24hours/BroCode/BroAudio/Main.java
+++ Java24/src/com/java24hours/BroCode/BroAudio/Main.java
@@ -2,6 +2,7 @@ package com.java24hours.BroCode.BroAudio;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -16,8 +17,31 @@ public class Main {
         Clip clip = AudioSystem.getClip();
         clip.open(audioStream);
 
-        clip.start();
+        String response = "";
 
-        String response = scanner.next();
+
+        while(!response.equals("Q")){
+            System.out.println("P = play, S = Stop, R = Reset, Q = Quit");
+            System.out.println("Enter your choice: ");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch(response){
+                case("P"): clip.start();
+                break;
+                case("S"): clip.stop();
+                break;
+                case("R"): clip.setMicrosecondPosition(0);
+                break;
+                case("Q"): clip.close();
+                break;
+                default:
+                    System.out.println("Not a valid response.");
+
+            }
+        }
+        System.out.println("Byeeeeee!");
+
     }
 }
